41f3d821efe63838f2f946d5ac20e95850fc5a4d
Andy <andypurbrick@gmail.com>
2019-03-20 10:40:06 +0000

    form fininshed

--- src/components/JourneyForm.js
+++ src/components/JourneyForm.js
@@ -1,60 +1,81 @@
 import React, { Component } from "react";
-import { STATION_DATA } from '../components/StationData';
-import { TimePicker } from "antd";
-import moment from "moment";
-import { Input } from "antd";
-import { DatePicker } from "antd";
-import { Button } from "antd";
-import { Select } from 'antd';
-const Search = Input.Search;
-const Option = Select.Option;
-
+import { STATION_DATA } from "../components/StationData";
+const API = "https://hsp-prod.rockshore.net/api/v1/serviceMetrics";
 
 export default class JourneyForm extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      from_loc: this.props.from_loc,
-      input: this.props.input,
-      // to_loc: this.props.,
-      // from_time: this.props.,
-      // to_time: this.props.,
-      // from_date: this.props.,
-      // to_date: this.props.,
-      // days: this.props.
-    }
+      from_loc: "",
+      to_loc: "",
+      from_time: "",
+      to_time: "",
+      from_date: "",
+      to_date: "",
+      day: "",
+      price: ""
+    };
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
+  handleTimeChange = event => {
+    const time = event.target.value
+    const newTime = time.replace(":","")
+    this.setState({ from_time: newTime })
+  }
+
+  handleDateChange = event => {
+    const date = new Date(event.target.value);
+    const day = date.getDay();
+    if (day < 6) {
+      this.setState({ day: "WEEKDAY" });
+    } else {
+      this.setState({ day: "WEEKEND" });
+    }
+    this.setState({ from_date: event.target.value })
+  };
+
+  createJourney = event => {
+    event.preventDefault();
+    const from_loc = this.state.from_loc;
+    const to_loc = this.state.to_loc;
+    const from_time = this.state.from_time;
+    const to_time = this.state.to_time;
+    const from_date = this.state.from_date;
+    const to_date = this.state.to_date;
+    const days = this.state.day;
+    const formData = {
+      from_loc,
+      to_loc,
+      from_time,
+      to_time,
+      from_date,
+      to_date,
+      days
+    };
+  };
+
+  journeyPostOne = formData => {
+    fetch("https://hsp-prod.rockshore.net/api/v1/serviceMetrics", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      Authorization: "Basic YW5keXB1cmJyaWNrQGdtYWlsLmNvbTpSZWdnYWUxOTc4Kg==",
+      body: JSON.stringify(formData)
+    }).then(resp => resp.json());
+  };
 
   render(props) {
-    const format = "HH:mm";
-    const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
-
-    onDateChange = (date, dateString) => {
-      console.log(date, dateString);
-    }
-
-    onTimeChange = (time, timeString) => {
-      console.log(time, timeString);
-    }
-
-    onFromChange = event => {
-      event.preventDefault()
-      this.setState({
-        input: event.target.value
-        })
-       this.filterFrom()
-      }
-
-    filterFrom = (input,option) => {
-      return stations.filter( station => station.Station.includes(this.input) )
-    }
-
-  }
-
-
+    const stations = STATION_DATA;
+    const optionItems = stations.map(station => (
+      <option value={station.TLC} key={station.TLC}>
+        {station.Station}
+      </option>
+    ));
 
     return (
       <form className="journey-form">
@@ -62,51 +83,54 @@ export default class JourneyForm extends Component {
           <h1>Enter your journey details</h1>
 
           <p>From</p>
-          <Select
-          showSearch
-          style={{ width: 200 }}
-          placeholder="Select a station"
-          optionFilterProp="stations"
-          onChange={event => this.props.onFromChange(event.target.value)}
-          filterOption={(input, option) => option.props.stations.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          <select
+            name="from_loc"
+            onChange={this.handleChange}
+            style={{ width: 200 }}
           >
-          <Option value={optionItems}>{optionItems}</Option>
-          </Select>
+            {optionItems}
+          </select>
 
           <p>To</p>
-          <Select
-          showSearch
-          style={{ width: 200 }}
-          placeholder="Select a station"
-          optionFilterProp="station"
-          onChange={event => this.props.onToChange(event.target.value)}
-          filterOption={(input, option) => option.props.stations.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          <select
+            name="to_loc"
+            onChange={this.handleChange}
+            style={{ width: 200 }}
           >
-          <Option value={optionItems}>{optionItems}</Option>
-          </Select>
+            {optionItems}
+          </select>
 
           <p>Scheduled Depature Time</p>
-          <TimePicker
-            defaultValue={moment("00:00", format)}
-            format={format}
+          <input
+            type="time"
+            onChange={this.handleTimeChange}
+            name="from_time"
             style={{ width: 200 }}
-            onChange={onTimeChange}
           />
 
           <p>Date</p>
-          <DatePicker onChange={onDateChange} style={{ width: 200 }} />
+          <input
+            type="date"
+            onChange={this.handleDateChange}
+            name="from_date"
+            style={{ width: 200 }}
+            data-date-format="yyyy-mm-dd"
+          />
 
           <p>Ticket Price</p>
-          <Input placeholder="Enter cost of ticket" style={{ width: 200 }} />
-
-          <p />
-          <Button
-            onClick={this.props.handleFormSubmit}
+          <input
+            name="to_date"
+            type="number"
+            onChange={this.handleChange}
+            placeholder="Enter cost of ticket"
             style={{ width: 200 }}
-            type="primary"
+          />
+          <button
+            onClick={e => this.props.createJourney(this.state, e)}
+            style={{ width: 200 }}
           >
-            Calculate refund >
-          </Button>
+            Calculate refund
+          </button>
         </div>
       </form>
     );
